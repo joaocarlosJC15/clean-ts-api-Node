5eaccf131193826266cc5e01e76dc95809a62542
João Carlos Alvim <jc15joaocarlos@gmail.com>
2020-05-31 17:54:33 -0300

    feat: ensure cors is enabled

---
+++ src/main/middlewares/cors.test.ts
@@ -0,0 +1,16 @@
+import request from 'supertest'
+import app from '../config/app'
+
+describe('CORS Middleware', () => {
+  test('Should enable CORS', async () => {
+    app.get('/test_cors', (request, response) => {
+      response.send()
+    })
+
+    await request(app)
+      .get('/test_cors')
+      .expect('acess-control-allow-origin', '*')
+      .expect('acess-control-allow-methods', '*')
+      .expect('acess-control-allow-headers', '*')
+  })
+})
