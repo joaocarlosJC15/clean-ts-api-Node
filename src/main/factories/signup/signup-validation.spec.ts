0ac0ae75ac5f2237dedba20ae3e80b47334480e1
João Carlos Alvim <jc15joaocarlos@gmail.com>
2020-06-14 22:59:41 -0300

    refactor: group validation imports in a single file

--- src/main/factories/signup/signup-validation.spec.ts
+++ src/main/factories/signup/signup-validation.spec.ts
@@ -1,10 +1,7 @@
 import { makeSignUpValidation } from './signup-validation'
-import { ValidationComposite } from '../../../presentation/helpers/validators/validation-composite'
-import { RequiredFieldValidation } from '../../../presentation/helpers/validators/required-field-validation'
 import { Validation } from '../../../presentation/protocols/validation'
-import { CompareFieldsValidation } from '../../../presentation/helpers/validators/compare-fields-validation'
-import { EmailValidation } from '../../../presentation/helpers/validators/email-validation'
 import { EmailValidator } from '../../../presentation/protocols/email-validator'
+import { EmailValidation, RequiredFieldValidation, ValidationComposite, CompareFieldsValidation } from '../../../presentation/helpers/validators'
 
 jest.mock('../../../presentation/helpers/validators/validation-composite')
 
