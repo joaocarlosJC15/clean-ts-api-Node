29f0ac7350d3e2de559a8e76461d4fb1106051a5
João Carlos Alvim <jc15joaocarlos@gmail.com>
2020-06-16 23:42:32 -0300

    feat: add LoginFactory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27018/clean-node-api',
-  port: process.env.PORT || 3333
+  port: process.env.PORT || 3333,
+  jwtSecret: process.env.JWT_SECRET || 'tj670==5H'
 }
