fa48fa5be1f4abd1ffeadcb9229ffa155ea82a48
João Carlos Alvim <jc15joaocarlos@gmail.com>
2020-09-22 15:36:35 -0300

    test: ensure DbLoadSurveyById throws if LoadSurveyByIdRepository throws

--- src/data/usecases/load-survey-by-id/db-load-survey-by-id.spec.ts
+++ src/data/usecases/load-survey-by-id/db-load-survey-by-id.spec.ts
@@ -69,9 +69,9 @@ describe('DbLoadSurveyById', () => {
     const { sut, loadSurveyByIdRepositoryStub } = makeSut()
 
     jest.spyOn(loadSurveyByIdRepositoryStub, 'loadById').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
-    
+
     const promise = sut.loadById('any_id')
-    
+
     await expect(promise).rejects.toThrow()
   })
 })
