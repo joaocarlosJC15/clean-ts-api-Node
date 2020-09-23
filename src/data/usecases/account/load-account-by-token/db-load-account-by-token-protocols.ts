d6fc49f02508ed82c247e4a7a977a13f9ad766f9
João Carlos Alvim <jc15joaocarlos@gmail.com>
2020-09-23 20:09:45 -0300

    refactor: organize project files

--- src/data/usecases/account/load-account-by-token/db-load-account-by-token-protocols.ts
+++ src/data/usecases/account/load-account-by-token/db-load-account-by-token-protocols.ts
@@ -1,4 +1,4 @@
 export { Decrypter } from '@/data/protocols/criptography/decrypter'
 export { AccountModel } from '@/domain/models/account'
 export { LoadAccountByTokenRepository } from '@/data/protocols/db/account/load-account-by-token-repository'
-export { LoadAccountByToken } from '@/domain/usecases/load-account-by-token'
+export { LoadAccountByToken } from '@/domain/usecases/account/load-account-by-token'
--- src/domain/usecases/account/load-account-by-token.ts
+++ src/domain/usecases/account/load-account-by-token.ts
@@ -1,4 +1,4 @@
-import { AccountModel } from '../models/account'
+import { AccountModel } from '../../models/account'
 
 export interface LoadAccountByToken {
   load (acessToken: string, role?: string): Promise<AccountModel>
