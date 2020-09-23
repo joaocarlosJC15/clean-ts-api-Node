a993ebe83228723d09ce76bcf9e6c75e4ea45b2e
João Carlos Alvim <jc15joaocarlos@gmail.com>
2020-09-23 19:13:18 -0300

    test: ensure DbSaveSurveyResult returns a SurveyResult on success

--- src/data/usecases/save-survey-result/db-save-survey-result.spec.ts
+++ src/data/usecases/save-survey-result/db-save-survey-result.spec.ts
@@ -68,4 +68,12 @@ describe('DbSaveSurveyResult Usecase', () => {
 
     await expect(promise).rejects.toThrow()
   })
+
+  test('Should return SurveyResult on success', async () => {
+    const { sut } = makeSut()
+
+    const surveyResult = await sut.save(makeFakeSurveyResultData())
+
+    expect(surveyResult).toEqual(makeFakeSurveyResult())
+  })
 })
