d155489646ba05b49393203086490143b45c1123
João Carlos Alvim <jc15joaocarlos@gmail.com>
2020-10-27 16:23:27 -0300

    docs: add login api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,8 +1,29 @@
+import { loginPath } from '../docs/path/login-path'
+import { accountSchema } from './schemas/account-schema'
+import { loginParamsSchema } from './schemas/login-params-schema'
+
 export default {
   openapi: '3.0.0',
   info: {
     title: 'Clean Node Api',
     description: 'Api do curso do Mango',
     version: '1.0'
+  },
+  servers: [
+    {
+      url: '/api'
+    }
+  ],
+  tags: [
+    {
+      name: 'Login'
+    }
+  ],
+  paths: {
+    '/login': loginPath
+  },
+  schemas: {
+    account: accountSchema,
+    loginParams: loginParamsSchema
   }
 }
---
+++ src/main/docs/schemas/account-schema.ts
@@ -0,0 +1,8 @@
+export const accountSchema = {
+  type: 'object',
+  properties: {
+    acessToken: {
+      type: 'string'
+    }
+  }
+}
