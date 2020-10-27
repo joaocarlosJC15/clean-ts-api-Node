840a0a24e2164ff203cef80c909927e14a045f66
João Carlos Alvim <jc15joaocarlos@gmail.com>
2020-10-26 22:02:57 -0300

    feat: add no cache middleware

---
+++ src/main/middlewares/no-cache.ts
@@ -0,0 +1,9 @@
+import { Request, Response, NextFunction } from 'express'
+
+export const noCache = (request: Request, response: Response, next: NextFunction): void => {
+  response.set('cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate')
+  response.set('Pragma', 'no-cache')
+  response.set('Expires', '0')
+  response.set('Surrogate-Control', 'no-store')
+  next()
+}
